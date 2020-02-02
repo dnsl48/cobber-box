3ce3e7ce774ede74a63e348f20d078b11f87a70e
Serge Latyntcev <dnsl48@gmail.com>
2020-02-03 11:59:38 +1300

    wip

--- service/php-7.1/cli/entrypoint.sh
+++ service/php-7.1/cli/entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-DIR="$(readlink -f \"$APP_DIR\")";
-
 if [ ! -z "$RUN_MODE_TEST" ] ; then
-    cd $DIR;
-    ./vendor/bin/phpunit $@;
+    cd $APP_DIR;
+    ./vendor/bin/phpunit $@
     exit;
 fi
 
-if [ ! -z "$RUN_MODE_PHPCS" ] ; then
-    cd $DIR;
-    ./vendor/bin/phpcs --standard=PSR12 bin/ src/ tests/ $@;
-    exit;
-fi
+cd $APP_DIR;
+./vendor/bin/$1 ${@:2}
 
 # $DIR/bin/cow $@
